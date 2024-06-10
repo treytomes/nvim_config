720dddf04a57125d28a09b60879afd59c1225c34
Trey Tomes <george.tomes@cityelectricsupply.com>
2024-06-10 10:40:56 -0500

    feat: treesitter config

--- init.lua
+++ init.lua
@@ -23,7 +23,8 @@ local plugins = {
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
     dependencies = { 'nvim-lua/plenary.nvim' }
-  }
+  },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
 }
 local opts = {}
 
@@ -35,6 +36,16 @@ require("lazy").setup(plugins, opts)
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+-- Treesitter config.
+local configs = require("nvim-treesitter.configs")
+configs.setup({
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "typescript", "html", "css" },
+  sync_install = false,
+  highlight = { enable = true },
+  indent = { enable = true },  
+})
+
 
 -- Setup the Catppuccin color scheme.
 require("catppuccin").setup()
