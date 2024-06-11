7d4987c7485717054c60f9c366f78b990073d310
Trey Tomes <george.tomes@cityelectricsupply.com>
2024-06-11 10:07:33 -0500

    feat: language servers

--- README.md
+++ README.md
@@ -4,4 +4,10 @@ My neovim configuration.
 ## Features
 
 * Tabs set to 2 spaces.
+* Catppuccin color scheme.
+* Lualine for the status line.
+* Neo-tree for the explorer-style view. (Ctrl+n to toggle)
+* Live grepping (<space>fg)
+* Search for file by name (Ctrl+p)
+* Autocomplete (Ctrl+x, Ctrl+o)
 
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,11 +2,27 @@
 -- Don't forget to `sudo apt install ripgrep fd-find` to install the dependencies, and `:checkhealth telescope` to ensure the plugin is fully functional.
 
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.6',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    local builtin = require("telescope.builtin")
-    vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-  end
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    end
+  },
+  {
+    'nvim-telescope/telescope-ui-select.nvim',
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+            }
+          }
+        }
+      })
+      require("telescope").load_extension("ui-select")
+    end
+  }
 }
